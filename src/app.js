2f0fc7f5308d5ee764103b7d7389b7d2ccf3e3ab
Sebastianignacioqa <sebastianignacioqa@gmail.com>
2021-07-29 00:54:07 +0000

    Excuses generator

--- src/app.js
+++ src/app.js
@@ -2,10 +2,54 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  var generarExcusa = () => {
+    var pronombre = ["El", "Mi", "Un", "Ese"];
+    var sujeto = [
+      "mimo",
+      "perro",
+      "gato",
+      "zorro",
+      "conejo",
+      "cantinero",
+      "chef"
+    ];
+    var verbo = ["tiro", "sacudio", "mojo", "mordio", "arreglo", "quemo"];
+    var cosa = [
+      "mi tarea",
+      "mi comida",
+      "la cama",
+      "mi laptop",
+      "la casa",
+      "la puerta",
+      "la cocina"
+    ];
+    var cuando = [
+      "hoy",
+      "ayer",
+      "antes de ayer",
+      "esta semana",
+      "el mes pasado",
+      "la semana pasada"
+    ];
+    var ipronombre = Math.floor(Math.random() * 4);
+    var isujeto = Math.floor(Math.random() * sujeto.length);
+    var iverbo = Math.floor(Math.random() * verbo.length);
+    var icosa = Math.floor(Math.random() * cosa.length);
+    var icuando = Math.floor(Math.random() * cuando.length);
+    return (
+      pronombre[ipronombre] +
+      " " +
+      sujeto[isujeto] +
+      " " +
+      verbo[iverbo] +
+      " " +
+      cosa[icosa] +
+      " " +
+      cuando[icuando]
+    );
+  };
+  document.querySelector("#excuse").innerHTML = generarExcusa();
 };
